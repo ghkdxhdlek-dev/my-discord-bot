f1b70376544ea3b69df7d139eef0f52f5fb836d8
ghkdxhdlek-dev <ghkdxhdlek@gmail.com>
2025-09-14 14:04:17 +0900

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,10 +5,10 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "✅ 봇이 살아있습니다!"
+    return "I'm alive"
 
 def run():
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host="0.0.0.0", port=3000)
 
 def keep_alive():
     t = Thread(target=run)
